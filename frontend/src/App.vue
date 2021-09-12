5183f137609403f5c14d0046f4fb88014e46319a
V-ampire <webjob010@gmail.com>
2021-09-12 16:49:51 +1000

    add frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="content"></div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    name: ''
+  }),
+  computed: {
+    showAlert() {
+      return this.name.length > 4 ? true : false
+    }
+  }
+}
+</script>
+
+<style>
+@import './assets/css/main.css';
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
